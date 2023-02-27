48c218ade1ee6daa4464d8a744db8fb59fd4903d
Loys Ollivier <loys.ollivier@arm.com>
2023-02-27 16:38:17 +0100

    Car: randomly generate VIN
    Generate VIN randomly. Letters are symmetrical.

--- mytool.py
+++ mytool.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
+import random
+import string
 
 class Car:
     """ A class that represents a car"""
 
-    def __init__(self, brand, model, vin, price):
+    def __init__(self, brand, model, price):
         self.brand = brand
         self.model = model
-        self.vin = vin
+        letters = ''.join(random.sample(string.ascii_uppercase,2))
+        number = random.randint(0,999)
+        self.vin = f"{letters}-{number:03}-{letters[::-1]}"
         self.price = price
 
     def __str__(self):
         return f"{self.brand:10} - {self.model:10} - {self.vin:10} - {self.price}"
 
 car_list = [
-    ["Peugeot", "2008", "AB-123-BA", "20000"],
-    ["Renault", "Megane", "ZZ-222-ZZ", "30000"],
-    ["Tesla", "Model 3", "DF-365-FD", "40000"],
-    ["Porsche", "Panamera", "FG-345-FD", "100000"],
-    ["Fiat", "Punto", "ER-234-RF", "8000"],
+    ["Peugeot", "2008", "20000"],
+    ["Renault", "Megane", "30000"],
+    ["Tesla", "Model 3", "40000"],
+    ["Porsche", "Panamera", "100000"],
+    ["Fiat", "Punto", "8000"],
 ]
 
 car_instances = []
-for (brand, model, vin, price) in car_list:
-    car_instances.append(Car(brand, model, vin, price))
+for (brand, model, price) in car_list:
+    car_instances.append(Car(brand, model, price))
     print(f"Created car: {car_instances[-1]}")
