e1c5e2410a4c35df211e68c5a68198ced5a6fe8a
Loys Ollivier <loys.ollivier@arm.com>
2023-02-27 11:29:55 +0100

    Create a Car class
    Create a Car class with some properties.

--- mytool.py
+++ mytool.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
-for i in range(10):
-    print(f"Hello {i}")
+class Car:
+    """ A class that represents a car"""
+
+    def __init__(self, brand, model, vin, price):
+        self.brand = brand
+        self.model = model
+        self.vin = vin
+        self.price = price
+
+car_list = [
+    ["Peugeot", "2008", "AB-123-BA", "20000"],
+    ["Renault", "Megane", "ZZ-222-ZZ", "30000"],
+    ["Tesla", "Model 3", "DF-365-FD", "40000"],
+    ["Porsche", "Panamera", "FG-345-FD", "100000"],
+]
+
+car_instances = []
+for (brand, model, vin, price) in car_list:
+    car_instances.append(Car(brand, model, vin, price))
+    print(f"Created car {vin}")
